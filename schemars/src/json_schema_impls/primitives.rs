6cf9343657388d0d2e1c4dbc1fc901e3ff1e9dde
Graham Esau <gesau@hotmail.co.uk>
2019-09-08 14:20:57 +0100

    Set `format` property on integers

--- schemars/src/json_schema_impls/primitives.rs
+++ schemars/src/json_schema_impls/primitives.rs
@@ -5,6 +5,12 @@ use serde_json::json;
 
 macro_rules! simple_impl {
     ($type:tt => $instance_type:ident) => {
+        simple_impl!($type => $instance_type, None);
+    };
+    ($type:tt => $instance_type:ident, $format: literal) => {
+        simple_impl!($type => $instance_type, Some($format.to_owned()));
+    };
+    ($type:tt => $instance_type:ident, $($format:tt)+) => {
         impl JsonSchema for $type {
             no_ref_schema!();
 
@@ -15,6 +21,7 @@ macro_rules! simple_impl {
             fn json_schema(_: &mut SchemaGenerator) -> Result {
                 Ok(SchemaObject {
                     instance_type: Some(InstanceType::$instance_type.into()),
+                    format: $($format)+,
                     ..Default::default()
                 }
                 .into())
@@ -26,20 +33,20 @@ macro_rules! simple_impl {
 simple_impl!(str => String);
 simple_impl!(String => String);
 simple_impl!(bool => Boolean);
-simple_impl!(f32 => Number);
-simple_impl!(f64 => Number);
-simple_impl!(i8 => Integer);
-simple_impl!(i16 => Integer);
-simple_impl!(i32 => Integer);
-simple_impl!(i64 => Integer);
-simple_impl!(i128 => Integer);
-simple_impl!(isize => Integer);
-simple_impl!(u8 => Integer);
-simple_impl!(u16 => Integer);
-simple_impl!(u32 => Integer);
-simple_impl!(u64 => Integer);
-simple_impl!(u128 => Integer);
-simple_impl!(usize => Integer);
+simple_impl!(f32 => Number, "float");
+simple_impl!(f64 => Number, "double");
+simple_impl!(i8 => Integer, "int8");
+simple_impl!(i16 => Integer, "int16");
+simple_impl!(i32 => Integer, "int32");
+simple_impl!(i64 => Integer, "int64");
+simple_impl!(i128 => Integer, "int128");
+simple_impl!(isize => Integer, "int");
+simple_impl!(u8 => Integer, "uint8");
+simple_impl!(u16 => Integer, "uint16");
+simple_impl!(u32 => Integer, "uint32");
+simple_impl!(u64 => Integer, "uint64");
+simple_impl!(u128 => Integer, "uint128");
+simple_impl!(usize => Integer, "uint");
 simple_impl!(() => Null);
 
 impl JsonSchema for char {
