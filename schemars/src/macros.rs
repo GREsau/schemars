317c317e950d6a1b2025341d559ebb04f15d8119
Graham Esau <gesau@hotmail.co.uk>
2019-10-20 14:34:15 +0100

    Make schema_for macro pattern stricter

--- schemars/src/macros.rs
+++ schemars/src/macros.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! schema_for {
-    ($($type:tt)+) => {
-        $crate::gen::SchemaGenerator::default().into_root_schema_for::<$($type)+>()
+    ($type:path) => {
+        $crate::gen::SchemaGenerator::default().into_root_schema_for::<$type>()
     };
 }
