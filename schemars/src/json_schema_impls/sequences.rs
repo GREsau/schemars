f23dc517c449aa0c4874b499c3db5ca2f3973ecf
Alastair Feille <me@alastair.se>
2020-12-01 19:35:43 -0600

    Make HashSet and BTreeSet use the uniqueItems keyword in their schema

--- schemars/src/json_schema_impls/sequences.rs
+++ schemars/src/json_schema_impls/sequences.rs
@@ -29,9 +29,38 @@ macro_rules! seq_impl {
     };
 }
 
+macro_rules! set_impl {
+    ($($desc:tt)+) => {
+        impl $($desc)+
+        where
+            T: JsonSchema,
+        {
+            no_ref_schema!();
+
+            fn schema_name() -> String {
+                format!("Set_of_{}", T::schema_name())
+            }
+
+            fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+                SchemaObject {
+                    instance_type: Some(InstanceType::Array.into()),
+                    array: Some(Box::new(ArrayValidation {
+                        unique_items: Some(true),
+                        items: Some(gen.subschema_for::<T>().into()),
+                        ..Default::default()
+                    })),
+                    ..Default::default()
+                }
+                .into()
+            }
+        }
+    };
+}
+
+
 seq_impl!(<T> JsonSchema for std::collections::BinaryHeap<T>);
-seq_impl!(<T> JsonSchema for std::collections::BTreeSet<T>);
-seq_impl!(<T, H> JsonSchema for std::collections::HashSet<T, H>);
+set_impl!(<T> JsonSchema for std::collections::BTreeSet<T>);
+set_impl!(<T, H> JsonSchema for std::collections::HashSet<T, H>);
 seq_impl!(<T> JsonSchema for std::collections::LinkedList<T>);
 seq_impl!(<T> JsonSchema for Vec<T>);
 seq_impl!(<T> JsonSchema for std::collections::VecDeque<T>);
