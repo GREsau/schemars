d01af3d61ad030fe80b25819015e040ab29c6397
Matt Campbell <mattcampbell@pobox.com>
2021-07-18 18:45:25 -0500

    Implement JsonSchema on EnumSet type

--- schemars/src/lib.rs
+++ schemars/src/lib.rs
@@ -269,6 +269,7 @@ Schemars can implement `JsonSchema` on types from several popular crates, enable
 - [`arrayvec`](https://crates.io/crates/arrayvec) (^0.5)
 - [`url`](https://crates.io/crates/url) (^2.0)
 - [`bytes`](https://crates.io/crates/bytes) (^1.0)
+- [`enumset`](https://crates.io/crates/enumset) (^1.0)
 */
 
 /// The map type used by schemars types.
