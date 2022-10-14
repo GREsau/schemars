3746adf8a23c8a35595965fdd039223d4f637740
Marcin Pajkowski <marcin.pajkowski@playbookengineering.com>
2022-10-14 15:34:14 +0200

    Docs processing - don't break markdown input

--- schemars_derive/src/attr/doc.rs
+++ schemars_derive/src/attr/doc.rs
@@ -1,76 +1,67 @@
 use syn::{Attribute, Lit::Str, Meta::NameValue, MetaNameValue};
 
 pub fn get_title_and_desc_from_doc(attrs: &[Attribute]) -> (Option<String>, Option<String>) {
-    let doc = match get_doc(attrs) {
-        None => return (None, None),
-        Some(doc) => doc,
-    };
+    let mut lines = get_lines(attrs);
 
-    if doc.starts_with('#') {
-        let mut split = doc.splitn(2, '\n');
-        let title = split
-            .next()
-            .unwrap()
-            .trim_start_matches('#')
-            .trim()
-            .to_owned();
-        let maybe_desc = split.next().and_then(merge_description_lines);
-        (none_if_empty(title), maybe_desc)
-    } else {
-        (None, merge_description_lines(&doc))
+    let title = lines
+        .get(0)
+        .as_ref()
+        .filter(|line| line.starts_with('#'))
+        .map(|line| line.trim_start_matches('#').trim().to_owned())
+        .and_then(none_if_empty);
+
+    if title.is_some() {
+        lines.remove(0);
+        pop_front_empty_lines(&mut lines);
     }
-}
 
-fn merge_description_lines(doc: &str) -> Option<String> {
-    let desc = doc
-        .trim()
-        .split("\n\n")
-        .filter_map(|line| none_if_empty(line.trim().replace('\n', " ")))
-        .collect::<Vec<_>>()
-        .join("\n\n");
-    none_if_empty(desc)
-}
-
-fn get_doc(attrs: &[Attribute]) -> Option<String> {
-    let attrs = attrs
-        .iter()
-        .filter_map(|attr| {
-            if !attr.path.is_ident("doc") {
-                return None;
-            }
-
-            let meta = attr.parse_meta().ok()?;
-            if let NameValue(MetaNameValue { lit: Str(s), .. }) = meta {
-                return Some(s.value());
-            }
-
-            None
-        })
-        .collect::<Vec<_>>();
-
-    let mut lines = attrs
-        .iter()
-        .flat_map(|a| a.split('\n'))
-        .map(str::trim)
-        .skip_while(|s| s.is_empty())
-        .collect::<Vec<_>>();
-
-    if let Some(&"") = lines.last() {
+    while let Some(true) = lines.last().map(|x| x.is_empty()) {
         lines.pop();
+    }
+
+    (title, none_if_empty(lines.join("\n")))
+}
+
+fn get_lines(attrs: &[Attribute]) -> Vec<String> {
+    let mut lines = vec![];
+    let mut all_starts_with_asterisk = true;
+
+    for attr in attrs.iter() {
+        if !attr.path.is_ident("doc") {
+            continue;
+        }
+
+        if let Ok(NameValue(MetaNameValue { lit: Str(s), .. })) = attr.parse_meta() {
+            let value = s.value();
+
+            for line in value.split('\n') {
+                let line = line.trim();
+
+                if !line.is_empty() {
+                    all_starts_with_asterisk = line.starts_with('*');
+                }
+
+                lines.push(line.to_owned());
+            }
+        }
     }
 
     // Added for backward-compatibility, but perhaps we shouldn't do this
     // https://github.com/rust-lang/rust/issues/32088
-    if lines.iter().all(|l| l.starts_with('*')) {
+    if all_starts_with_asterisk {
         for line in lines.iter_mut() {
-            *line = line[1..].trim()
+            *line = line.trim_start_matches('*').trim().to_owned();
         }
-        while let Some(&"") = lines.first() {
-            lines.remove(0);
-        }
-    };
+    }
+    pop_front_empty_lines(&mut lines);
 
-    none_if_empty(lines.join("\n"))
+    lines
+}
+
+fn pop_front_empty_lines(lines: &mut Vec<String>) {
+    while let Some(true) = lines.first().map(|x| x.is_empty()) {
+        lines.remove(0);
+    }
 }
 
 fn none_if_empty(s: String) -> Option<String> {
