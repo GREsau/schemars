01545b28d243236d6d67225938ca65c01abf9980
Jørgen Kristensen <jk@bitnor.org>
2023-03-09 11:17:14 +0100

    implement JsonSchema trait for time::OffsetDateTime

--- schemars/src/lib.rs
+++ schemars/src/lib.rs
@@ -261,6 +261,7 @@ println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 
 Schemars can implement `JsonSchema` on types from several popular crates, enabled via feature flags (dependency versions are shown in brackets):
 - `chrono` - [chrono](https://crates.io/crates/chrono) (^0.4)
+- `time` - [time](https://crates.io/crates/time) (^0.3)
 - `indexmap1` - [indexmap](https://crates.io/crates/indexmap) (^1.2)
 - `either` - [either](https://crates.io/crates/either) (^1.3)
 - `uuid08` - [uuid](https://crates.io/crates/uuid) (^0.8)
