c87ca6867d1e849540f1ba69ddc8dfcd3612712e
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2023-05-28 17:02:31 -0500

    _

--- schemars/src/json_schema_impls/enumflags2.rs
+++ schemars/src/json_schema_impls/enumflags2.rs
@@ -3,4 +3,31 @@ use crate::schema::*;
 use crate::JsonSchema;
 use enumflags2::{BitFlags, _internal::RawBitFlags};
 
+impl<T> JsonSchema for BitFlags<T> where T: JsonSchema + RawBitFlags {
+    fn is_referenceable() -> bool {
+        true
+    }
+
+    fn schema_name() -> String {
+        format!("BitFlags_{}", T::schema_name())
+    }
+
+    fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+        let t = gen.subschema_for::<T>();
+        let r = u64::json_schema(gen);
+        match r {
+            Schema::Object(mut o) => {
+               match t {
+                   Schema::Object(mut to) => {
+                      o.metadata = to.metadata;
+                   },
+                   _ => ()
+               }
+            },
+            _ => ()
+        }
+        r
+    }
+}
+
 forward_impl!((<T> JsonSchema for BitFlags<T> where T: RawBitFlags) => u64);
