fd5b6c670aee2f40c867c0a7619d18cb8cc11939
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2023-05-28 20:31:23 -0500

    _

--- schemars/src/json_schema_impls/netidx.rs
+++ schemars/src/json_schema_impls/netidx.rs
@@ -1,7 +1,8 @@
 use crate::gen::SchemaGenerator;
 use crate::schema::*;
 use crate::JsonSchema;
-use netidx_core::{path::Path, chars::Chars};
+use netidx_core::{path::Path, chars::Chars, pool::{Poolable, Pooled}};
 
 forward_impl!((JsonSchema for Path) => String);
 forward_impl!((JsonSchema for Chars) => String);
+forward_impl!((<T> JsonSchema for Pooled<T> where T: Poolable + Sync + Send + JsonSchema + 'static) => T);
