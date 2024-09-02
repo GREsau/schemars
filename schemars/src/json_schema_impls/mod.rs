600bc20484e3a92ed8db211f1d7bbbac87cf4f86
novacrazy <novacrazy@gmail.com>
2024-09-02 12:57:00 -0500

    Fix feature name

--- schemars/src/json_schema_impls/mod.rs
+++ schemars/src/json_schema_impls/mod.rs
@@ -84,8 +84,8 @@ mod indexmap2;
 #[cfg(feature = "semver1")]
 mod semver1;
 
-#[cfg(feature = "triomphe")]
-forward_impl!((<T: ?Sized> crate::JsonSchema for triomphe::Arc<T> where T: crate::JsonSchema) => T);
+#[cfg(feature = "triomphe01")]
+forward_impl!((<T: ?Sized> crate::JsonSchema for triomphe01::Arc<T> where T: crate::JsonSchema) => T);
 
 #[cfg(feature = "smallvec1")]
 forward_impl!((<A: smallvec1::Array> crate::JsonSchema for smallvec1::SmallVec<A> where A::Item: crate::JsonSchema) => alloc::vec::Vec<A::Item>);
