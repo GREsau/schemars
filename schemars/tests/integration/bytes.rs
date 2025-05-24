6a089fa95ba7bc882d10901a099a046e1f4e8967
Graham Esau <gesau@hotmail.co.uk>
2025-05-24 22:22:21 +0100

    Set `minimum`/`maximum` for i8/u8/i16/u16 (#401)

--- schemars/tests/integration/bytes.rs
+++ schemars/tests/integration/bytes.rs
@@ -6,19 +6,10 @@ fn bytes() {
     test!(Bytes)
         .assert_snapshot()
         .assert_allows_ser_roundtrip([Bytes::new(), Bytes::from_iter([12; 34])])
-        .assert_matches_de_roundtrip(arbitrary_values_except(
-            is_array_of_u64,
-            "FIXME schema allows out-of-range positive integers",
-        ));
+        .assert_matches_de_roundtrip(arbitrary_values());
 }
 
 #[test]
 fn bytes_mut() {
     test!(BytesMut).assert_identical::<Bytes>();
-}
-
-fn is_array_of_u64(value: &Value) -> bool {
-    value
-        .as_array()
-        .is_some_and(|a| a.iter().all(Value::is_u64))
 }
