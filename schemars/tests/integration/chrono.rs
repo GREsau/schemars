63ee89a2fcd5e9ef45e26d7ae6d4b80985ec6b54
Graham Esau <gesau@hotmail.co.uk>
2025-06-16 10:17:18 +0100

    Impl `JsonSchema` for `chrono::TimeDelta`

--- schemars/tests/integration/chrono.rs
+++ schemars/tests/integration/chrono.rs
@@ -1,5 +1,5 @@
 use crate::prelude::*;
-use chrono04::prelude::*;
+use chrono04::{prelude::*, TimeDelta};
 
 #[derive(JsonSchema, Serialize, Deserialize)]
 struct ChronoTypes {
@@ -8,6 +8,7 @@ struct ChronoTypes {
     naive_date: NaiveDate,
     naive_date_time: NaiveDateTime,
     naive_time: NaiveTime,
+    time_delta: TimeDelta,
 }
 
 #[test]
@@ -20,14 +21,20 @@ fn chrono() {
 
     test!(DateTime<Utc>)
         .assert_allows_ser_roundtrip_default()
+        // JSON Schema only allows dates with 4-digit years
+        // .assert_allows_ser_roundtrip([DateTime::<Utc>::MIN_UTC, DateTime::<Utc>::MAX_UTC])
         .assert_matches_de_roundtrip(arbitrary_values());
 
     test!(NaiveDate)
         .assert_allows_ser_roundtrip_default()
+        // JSON Schema only allows dates with 4-digit years
+        // .assert_allows_ser_roundtrip([NaiveDate::MIN, NaiveDate::MAX])
         .assert_matches_de_roundtrip(arbitrary_values());
 
     test!(NaiveDateTime)
         .assert_allows_ser_roundtrip_default()
+        // JSON Schema only allows dates with 4-digit years
+        // .assert_allows_ser_roundtrip([NaiveDateTime::MIN, NaiveDateTime::MAX])
         .assert_matches_de_roundtrip(arbitrary_values_except(
             Value::is_string,
             "Custom format 'partial-date-time', so arbitrary strings technically allowed by schema",
@@ -35,8 +42,15 @@ fn chrono() {
 
     test!(NaiveTime)
         .assert_allows_ser_roundtrip_default()
+        .assert_allows_ser_roundtrip([NaiveTime::MIN, NaiveDateTime::MAX.time()])
         .assert_matches_de_roundtrip(arbitrary_values_except(
             Value::is_string,
             "Custom format 'date-time', so arbitrary strings technically allowed by schema",
         ));
+
+    test!(TimeDelta)
+        .assert_allows_ser_roundtrip_default()
+        .assert_allows_ser_roundtrip([TimeDelta::MIN, TimeDelta::MAX])
+        .assert_rejects_de([json!([0, -1]), json!([0, 1_000_000_000])])
+        .assert_matches_de_roundtrip(arbitrary_values());
 }
